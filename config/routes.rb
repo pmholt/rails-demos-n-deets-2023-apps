848ed81d221cdbd8d93abe213d60f0c6af566930
Peyton Holt <pmholt@memphis.edu>
2023-09-06 14:24:34 -0500

    mistake

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html  
   get 'home', to: 'pages#home', as: 'home'
   # Defines the root path route ("/")
   # root "articles#index"
